7c59a07461092b6315ea5a7582f90b4a5faed77a
Daniar Meiradi <daniarmeiradi@MacBook-Air.local>
2017-07-24 19:14:41 +0700

    roman_numerals_recursive done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,15 @@
-function to_roman(input) {
+function to_roman(input, result='') {
   // start your code here
-  return to_roman(input); 
+  let digit = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+  let roman = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+
+  for(let i=0;i<digit.length;i++){
+    while(input % digit[i] < input){
+      result += roman[i];
+      return to_roman(input - digit[i], result);
+    }
+  }
+  return result;
 }
 
 console.log('My totally sweet testing script for new roman\n')
